45bc9e589b739ccfa8460662b9a9f22e5ca43728
Joris Pennings <joris.pennings@cm.com>
2022-05-31 15:03:33 +0200

    Update text_client.py
    Updated the docstrings for the text_client

--- cm_text/text_client.py
+++ cm_text/text_client.py
@@ -16,42 +16,80 @@ class TextClient:
     MESSAGES_MAXIMUM = 100
     VERSION = __version__
 
-    # Initialize Client with default Gateway Gateways.Global
     def __init__(self, apikey, gateway=Gateways.Global):
         self.apikey = apikey
         self.gateway = gateway
 
-    # Send 1 message to one or multiple locations
     def SendSingleMessage(self, message, from_, to=[], reference=None, allowedChannels=['SMS']):
+        """
+        Method that sends a single message.
+
+        :param str message: The Message to be send
+        :param str from_: Sender
+        :param list to: List of strings containing all recipient phone numbers
+        :param str reference: The message reference which can be provided by the customer in the outbound message
+        :param list allowedChannels: list of strings containing allowed channels to be used.
+        """
         self.messages.append(Message(message, from_=from_, to=to, reference=reference, allowedChannels=allowedChannels))
         response = self.send()
         return response
 
-    # Add a message to the list
     def AddMessage(self, message, from_='', to=[], reference=None, allowedChannels=['SMS']):
+        """
+        Method that appends a default message to the list of messages.
+
+        :param str message: The Message to be send
+        :param str from_: Sender
+        :param list to: List of strings containing all recipient phone numbers
+        :param str reference: The message reference which can be provided by the customer in the outbound message
+        :param list allowedChannels: list of strings containing allowed channels to be used.
+        """
         self.messages.append(Message(message, from_=from_, to=to, reference=reference, allowedChannels=allowedChannels))
 
-    # Add a rich message to the list
     def AddRichMessage(self, message, media, from_='', to=[], reference=None, allowedChannels=['Whatsapp']):
+        """
+        Method that appends a rich content message to the list of messages.
+
+        :param str message: The Message to be send
+        :param dict media: Dictionary Containing mediaName, mediaUri, mimeType
+        :param str from_: Sender
+        :param list to: List of strings containing all recipient phone numbers
+        :param str reference: The message reference which can be provided by the customer in the outbound message
+        :param list allowedChannels: list of strings containing allowed channels to be used.
+        """
         self.messages.append(
             Message(message, media=media, from_=from_, to=to, reference=reference, allowedChannels=allowedChannels))
 
-    # Add a Whatsapp Template message to the list
     def AddWhatsappTemplateMessage(self, template, from_='', to=[], reference=None, media=None):
-        self.messages.append(Message(media=media,
-                                     from_=from_,
-                                     to=to,
-                                     reference=reference,
-                                     allowedChannels=['Whatsapp'],
-                                     template=template))
+        """
+        Method that appends a whatsapp template message to the list of messages.
 
-    # Add an Interactive Whatsapp message to the list
+        :param WhatsappTemplate template: WhatsappTemplate class
+        :param str from_: Sender
+        :param list to: List of strings containing all recipient phone numbers
+        :param str reference: The message reference which can be provided by the customer in the outbound message
+        :param dict media: Dictionary Containing mediaName, mediaUri, mimeType
+        """
+        self.messages.append(
+            Message(media=media, from_=from_, to=to, reference=reference, allowedChannels=['Whatsapp'], template=template))
+
     def add_whatsapp_interactive_message(self, interactive, body='', from_='', to=[], reference=None):
+        """
+        Method that appends an interactive whatsapp message to the list of messages.
+
+        :param InteractiveMessage interactive: InteractiveMessage class
+        :param str body: Fallback text
+        :param str from_: Sender
+        :param list to: List of strings containing all recipient phone numbers
+        :param str reference: The message reference which can be provided by the customer in the outbound message
+        """
         self.messages.append(
             Message(to=to, body=body, from_=from_, reference=reference, allowedChannels=['Whatsapp'], interactive=interactive))
 
-    # Validation if amount of messages dont exceed maximum or equals 0
     def _validate_messages(self, messages, maximum):
+        """
+        Method that validates amount of messages.
+        """
         if len(messages) == 0:
             print('No messages in the queue')
             return False
@@ -60,8 +98,10 @@ class TextClient:
             return False
         return True
 
-    # Send all messages in the list
     def send(self):
+        """
+        Method that validates message count, constructs post request to be send to (selected) gateway.
+        """
         if self._validate_messages(self.messages, self.MESSAGES_MAXIMUM):
             # Set data for post
             data = self.encodeData(self.messages)
@@ -85,8 +125,10 @@ class TextClient:
             # Return response
             return response
 
-    # Method to encode Data, Gateway accepts this format
     def encodeData(self, messages):
+        """
+        Method that encodes all messages to (gateway) json object to be send as data in post request.
+        """
         # Set productToken
         data = {"messages": {"authentication": {"producttoken": self.apikey}}}
         data['messages']['msg'] = []
@@ -108,7 +150,15 @@ class TextClient:
                     }
                     }
 
-            # If message is template
+            # If reference is set, add it to temp.
+            if message.reference is not None:
+                temp["reference"] = message.reference
+
+            # If customgrouping3 is set, add it to temp.
+            if message.customgrouping3 is not None:
+                temp["customGrouping3"] = message.customgrouping3
+
+            # If message is template, set richContent key in temp
             if message.template is not None:
                 temp["richContent"] = {
                     "conversation": [{
@@ -129,7 +179,7 @@ class TextClient:
                     }]
                 }
 
-            # If message is rich and no template
+            # If message is rich and no template, set richContent key in temp
             if message.template is None and message.richContent is not None:
                 temp["richContent"] = {
                     "conversation": [{
